1bcbfec733d5129a952a2c36340ae69fb3bab2b1
Omid Chowdhury <omidchowdhury@gmail.com>
2015-12-02 13:56:34 +0600

    47335_Master layout & home for trainee

---
+++ resources/views/layouts/trainee.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+@section('menu_left')
+<ul class="nav navbar-nav">
+    <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Course <span class="caret"></span></a>
+        <ul class="dropdown-menu">
+            <li>{!! Html::link('#', 'Course Details') !!}
+            </li>
+            <li>{!! HTML::link('#', 'Course Members') !!}
+            </li>
+        </ul>
+    </li>
+    <li>{!! HTML::link('#', 'Calender') !!}
+    </li>
+    <li>{!! HTML::link('#', 'Report') !!}
+    </li>
+    <li>{!! HTML::link('#', 'Progress') !!}
+    </li>
+    <li>{!! HTML::link('#', 'History') !!}
+    </li>
+    <ul class="nav navbar-nav navbar-right">
+        <li id="profile-right" class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">{{ Auth::user()->name }}<span class="caret"></span></a>
+            <ul class="dropdown-menu">
+                <li>{!! HTML::link('#', 'Settings') !!}
+                </li>
+                <li>{!! HTML::link('auth/logout', 'Logout') !!}
+                </li>
+            </ul>
+        </li>
+    </ul>
+</ul>
+@endsection
---
+++ resources/views/trainee/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.trainee')
+@section('content')
+<center>
+    <img src="/img/welcome.jpeg">
+    <strong>
+	    <h1>Enlighten Your Future</h1>
+    </strong>
+</center>
+@endsection
