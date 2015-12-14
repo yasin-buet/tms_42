5f53f8ab3937054728cbfca049c63c7433436e37
yasin-buet <yasin.buet@gmail.com>
2015-12-14 10:51:47 +0600

    47336_trainee_view_progress_of_course

--- resources/views/trainee/courses/show.blade.php
+++ resources/views/trainee/courses/show.blade.php
@@ -17,5 +17,14 @@
         @foreach ($course->subjects as $subject)
             <h1><span class="label label-default">{!! $subject->name !!}</span></h1>
         @endforeach
+        @if (!empty($courseProgress))
+            <h2> You are enrolled to this course !! Your Current Progress </h2>
+                <div class="progress">
+                    <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width:{{ $courseProgress }}%">Course Progress({{ $courseProgress }} % )
+                    </div>
+                </div>
+            <h2>Course Start Date : {{ $course->start_date }}</h2>
+            <h2>Course End date : {{ $course->end_date }}</h2>
+        @endif
 </div>
 @endsection
