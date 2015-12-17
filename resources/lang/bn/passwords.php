4d3187b5f1e1e3dae800d22ce7d0effe6543d8df
asifbuetcse <asifbuetcse@yahoo.com>
2015-12-17 15:10:16 +0600

    48835_localization_and_css_issues

---
+++ resources/lang/bn/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reminder Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'পাসওয়ার্ড অন্তত ছয় অক্ষরের হবে এবং নিশ্চিতকরণ মেলানো',
+    'reset' => 'Your password has been reset!',
+    'sent' => 'We have e-mailed your password reset link!',
+    'token' => 'This password reset token is invalid.',
+    'user' => "We can't find a user with that e-mail address.",
+
+];
--- resources/views/supervisor/courses/index.blade.php
+++ resources/views/supervisor/courses/index.blade.php
@@ -1,15 +1,36 @@
 @extends('layouts.supervisor')
 @section('content')
-<div id="div-indent">
-    <h1>Course List </h1>
-    @foreach ($courses as $course)
-        <h1><span class="label label-default">{!! link_to_route('supervisor.courses.show', $course->name, [$course->id]) !!}</span></h1>
-        {!! link_to_route('supervisor.courses.edit', 'Edit This Course', [$course->id]) !!}
-        {!! link_to_route('supervisor.course.users.index', 'Edit Trainees', [$course->id]) !!} 
-        {!! Form::open(['route' => ['supervisor.courses.destroy', $course->id], 'method' => 'DELETE']) !!}
-            {!! Form::submit('Delete This Course') !!}
-        {!! Form::close() !!}
-    @endforeach
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-primary">
+                <div class="panel-body">
+                    <ul class="chat">
+                        @foreach ($courses as $course)
+                            <li class="list-group">
+                                <div class="panel">
+                                    <div class="header">
+                                        <strong class="primary-font"><h1>{!! link_to_route('supervisor.courses.show', $course->name, [$course->id]) !!}</h1></strong>
+                                    </div>
+                                    <p>
+                                        {!! link_to_route('supervisor.courses.edit', 'Edit This Course', [$course->id], ['class' => 'btn btn-success']) !!}
+                                    </p>
+                                    <p>
+                                        {!! link_to_route('supervisor.course.users.index', 'Edit Trainees', [$course->id], ['class' => 'btn btn-primary']) !!} 
+                                    </p>
+                                    <p>
+                                        {!! Form::open(['route' => ['supervisor.courses.destroy', $course->id], 'method' => 'DELETE']) !!}
+                                            {!! Form::submit('Delete This Course', ['class' => 'btn btn-large btn-danger openbutton']) !!}
+                                        {!! Form::close() !!}
+                                    </p>
+                                </div>
+                            </li>
+                        @endforeach
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 <center>
     {!! $courses->render() !!}
